7dce5ae87ff04223f777371328af73745cbeb0cb
kostya3004 <110536627+kostya3004@users.noreply.github.com>
2023-01-21 21:23:04 +0000

    Deploying to gh-pages from @ kostya3004/goit-js-hw-09@45e71c1d748b2df9541c847219d33a251584e0e8 🚀

---
+++ 01-color-switcher.46924e44.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(d)})),e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.46924e44.js.map
