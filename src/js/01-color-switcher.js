45e71c1d748b2df9541c847219d33a251584e0e8
kostya3004 <110536627+kostya3004@users.noreply.github.com>
2023-01-21 22:22:31 +0100

    01 switcher rdy

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector("[data-start]")
+const stopBtn = document.querySelector("[data-stop]")
+let timerId = null
 
+startBtn.addEventListener("click", startBtnClick);
+stopBtn.addEventListener("click", stopBtnClick);
+
+stopBtn.disabled = true
+
+function startBtnClick() {
+    startBtn.disabled = true
+    stopBtn.disabled = false
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopBtnClick() {
+    startBtn.disabled = false
+    stopBtn.disabled = true
+    clearInterval(timerId)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
